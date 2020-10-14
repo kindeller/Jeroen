68c128acb1558c564ce3765322235e22422e9ae9
agyputri <20026885@tafe.wa.edu.au>
2020-10-14 15:53:32 +0800

    homepage is now mobile responsive

--- parts/content-offcanvas.php
+++ parts/content-offcanvas.php
@@ -7,6 +7,13 @@
 ?>
 
 <div class="off-canvas position-right" id="off-canvas" data-off-canvas>
+	<br>
+	<br>
+	 <button class="close-button" aria-label="Close alert" type="button" data-close>
+    <span aria-hidden="true">&times;</span>
+  </button>
+
+	<div class="mobile-menu">
 	<?php joints_off_canvas_nav(); ?>
 
 	<?php if ( is_active_sidebar( 'offcanvas' ) ) : ?>
@@ -14,5 +21,6 @@
 		<?php dynamic_sidebar( 'offcanvas' ); ?>
 
 	<?php endif; ?>
+	</div>
 
 </div>
--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -7,7 +7,7 @@
 ?>
 
 <div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
+	<div class="top-bar-left float-left show-for-medium">
 		<!--<ul class="menu">
 			<li><a href="<?php echo home_url(); ?>"><img src="wp-content/themes/verschoor-vision-imagination/assets/images/logo.png" width="100" height="150" alt="logo"></a></li>
 		</ul>-->
@@ -19,10 +19,12 @@
 	</div>
 	
 	<div class="top-bar-right float-right show-for-small-only">
-		<ul class="menu">
+		<!--<ul class="menu">
 			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
-			<!--<li><a data-toggle="off-canvas"><?php //_e( 'Menu', 'jointswp' ); ?></a></li>-->
-		</ul>
+			
+			<!--<li><a data-toggle="off-canvas"><?php //_e( 'Menu', 'jointswp' ); ?></a></li>
+		</ul>-->
+		<img src="wp-content/themes/verschoor-vision-imagination/assets/images/logo.png" width="100" height="150" alt="logo" data-toggle="off-canvas"></a>
 	</div>
 	
 	
